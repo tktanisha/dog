84ba8e0a1036c6f388ecdf03e3a7a1d1fed37d4b
Tanisha Khandelwal <Tanisha.Khandelwal@watchguard.com>
2025-07-30 13:43:22 +0530

    first commit

---
+++ main.go
@@ -0,0 +1,12 @@
+package main
+
+import "fmt"
+
+func Greet(str string) string {
+	return fmt.Sprintf("Hello, " + str)
+
+}
+func main() {
+
+	fmt.Println("This is the dog package main.go file.")
+}
