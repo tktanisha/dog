c7082d8c0b8a0278697455e188885b2e1f00b24f
Tanisha Khandelwal <Tanisha.Khandelwal@watchguard.com>
2025-07-30 15:41:46 +0530

    version v1.1.0

--- main.go
+++ main.go
@@ -10,3 +10,7 @@ func Greet(str string) string {
 func Bark(Str string) string {
 	return fmt.Sprintf(" %s is doing bhow bhow", Str)
 }
+
+func meow(Str string) string {
+	return fmt.Sprintf(" %s is doing meow meow", Str)
+}
